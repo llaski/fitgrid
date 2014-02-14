baaa4601a8f998a10bcccb2af2172c13cf9d143d
Larry Laski <larry.laski@gmail.com>
2014-02-13 23:35:33 -0500

    init

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+# project_path = "."
+css_dir = "public/css"
+sass_dir = "lib/sass"
+images_dir = "img"
+
+# output_style = :compressed
+output_style = :compact
+relative_assets = true
+line_comments = false
---
+++ routes/photos.js
@@ -0,0 +1,5 @@
+exports.get = function(db) {
+	return function (req, res) {
+
+	};
+}
